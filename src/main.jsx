913c2e3d025d08ed5d4edff02c107e63171297c8
bakhtawarneaz <bakhtawarneaz@gmail.com>
2024-11-30 12:32:31 +0500

    cva-new

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './redux/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './style/main.css'
+import Color from '@helper/Color';
 
 const queryClient = new QueryClient();
 
@@ -22,6 +23,7 @@ createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
             <PersistGate persistor={persistor}>
+                <Color />
                 <Toaster position="top-center" toastOptions={toastOptions} />
                 <RouterProvider router={router} />
             </PersistGate>
